606bc3f35b10b2777fe8f07f488d5f73fc41db7c
Ivan Stoianov <ivan.stoianov@sap.com>
2023-09-24 17:51:33 +0300

    removed unused method

--- src/main/java/com/parovsky/traver/repository/CategoryRepository.java
+++ src/main/java/com/parovsky/traver/repository/CategoryRepository.java
@@ -12,6 +12,4 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 	List<Category> findAllByLocationsFollowersEmail(String email);
 
 	boolean existsByName(String name);
-
-	boolean existsByNameAndIdNot(String name, Long id);
 }
